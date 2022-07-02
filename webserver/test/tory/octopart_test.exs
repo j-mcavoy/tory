087165fe69139e1e4c99e00d0c84e68aae62b70c
John McAvoy <j-mcavoy@protonmail.com>
2022-07-02 12:26:41 -0700

    add octopart fetch tests

--- webserver/test/tory/octopart_test.exs
+++ webserver/test/tory/octopart_test.exs
@@ -2,12 +2,34 @@ defmodule Tory.OctopartTest do
   use Tory.DataCase
 
   describe "octopart" do
-    alias Tory.Octopart
+    import Tory.Octopart
     alias Tory.Part.Part
 
     test "fetch_meta_from_octopart with only mpn works" do
-      part = %Part{mpn: "some mpn"}
-      resp = octopart_api_fetch(part)
+      part = %Part{mpn: "1N914-T50A"}
+      {ok?, %{body: body}} = fetch_meta_from_octopart(part)
+      assert ok? == :ok
+      assert not Map.has_key?(body, :errors)
+      IO.inspect(body)
+    end
+
+    test "fetch_meta_from_octopart with only octopart_id" do
+      part = %Part{octopart_id: "46505370"}
+      {ok?, %{body: body}} = fetch_meta_from_octopart(part)
+      assert ok? == :ok
+      assert not Map.has_key?(body, :errors)
+      IO.inspect(body)
+    end
+
+    test "fetch_meta_from_octopart with octopart_id and mpn uses octopart_id" do
+      part = %Part{mpn: "1N914-T50A", octopart_id: "46505370"}
+      {ok?, %{body: body}} = fetch_meta_from_octopart(part)
+      assert ok? == :ok
+      assert not Map.has_key?(body, :errors)
+      %{data: %{parts: parts}} = body
+      assert length(parts) == 1
+      octopart = hd(parts)
+      assert part.octopart_id == octopart.id
     end
   end
 end
